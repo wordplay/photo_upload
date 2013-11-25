6e3ae4e1e27599aee6ad408a3e25088344c84925
Adam Horne <ahorne@wordplaysoftware.com>
2013-11-25 12:36:11 -0500

    Changed module name to PhotoUpload

--- Rakefile
+++ Rakefile
@@ -3,4 +3,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-TemplateBootstrapSite::Application.load_tasks
+PhotoUpload::Application.load_tasks
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-TemplateBootstrapSite::Application.configure do
+PhotoUpload::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
